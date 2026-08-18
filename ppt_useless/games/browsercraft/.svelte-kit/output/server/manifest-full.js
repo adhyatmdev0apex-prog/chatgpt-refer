32c6419b6e7647b4939cfe8fe035ad55081019c7
adhyatmdev0apex-prog <adhyatm.dev0apex@gmail.com>
2026-08-18 11:38:21 +0000

    add minecraft java(Build)

---
+++ ppt_useless/games/browsercraft/.svelte-kit/output/server/manifest-full.js
@@ -0,0 +1,36 @@
+export const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.ico","fonts/minecrafter/License.png","fonts/minecrafter/License.txt","fonts/minecrafter/Minecrafter.Reg.woff2","fonts/minecrafter/Read Me.txt","lwjgl/libraries/gl4es.wasm","lwjgl/libraries/liblwjgl.so","lwjgl/libraries/lwjgl.js","lwjgl/lwjgl-2.9.3.jar","lwjgl/lwjgl_util-2.9.3.jar"]),
+	mimeTypes: {".png":"image/png",".txt":"text/plain",".woff2":"font/woff2",".wasm":"application/wasm",".so":"application/octet-stream",".js":"text/javascript",".jar":"application/java-archive"},
+	_: {
+		client: {start:"_app/immutable/entry/start.DYQ5lDYj.js",app:"_app/immutable/entry/app.BjH-z49U.js",imports:["_app/immutable/entry/start.DYQ5lDYj.js","_app/immutable/chunks/AZQZkTMr.js","_app/immutable/chunks/DtqzF-ji.js","_app/immutable/chunks/DMaBcy5s.js","_app/immutable/entry/app.BjH-z49U.js","_app/immutable/chunks/DtqzF-ji.js","_app/immutable/chunks/CZuMPPrw.js","_app/immutable/chunks/BBBJ55oA.js","_app/immutable/chunks/DMaBcy5s.js","_app/immutable/chunks/Bhuhb2TW.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		nodes: [
+			__memo(() => import('./nodes/0.js')),
+			__memo(() => import('./nodes/1.js')),
+			__memo(() => import('./nodes/2.js'))
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			}
+		],
+		prerendered_routes: new Set([]),
+		matchers: async () => {
+			
+			return {  };
+		},
+		server_assets: {}
+	}
+}
+})();
